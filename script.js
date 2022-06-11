7e49215b59eb73b9dafa476a2af8990572e60283
Eren Gazioglu <pompyproduction@gmail.com>
2022-06-11 16:45:17 +0100

    Add JS hello world

---
+++ script.js
@@ -0,0 +1,7 @@
+const container = document.querySelector("#output-box");
+
+const content = document.createElement("h3");
+content.textContent = "This is an h3 element.";
+
+container.appendChild(content);
+
