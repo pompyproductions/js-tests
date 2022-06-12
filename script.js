26d5d1e24ff8d62707e2bc94054277cbf0fa14f9
Eren Gazioglu <pompyproduction@gmail.com>
2022-06-12 15:30:44 +0100

    Add content through JS

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-const container = document.querySelector("#output-box");
+function addNewElement(tag, content="", parent=container, ...classes) {
+    let newElement = document.createElement(tag);
+    newElement.textContent = content;
+    classes.forEach(element => {
+        newElement.classList.add(element)
+    });
+    parent.appendChild(newElement);
 
-const content = document.createElement("h3");
-content.textContent = "This is an h3 element.";
+}
 
-container.appendChild(content);
+const container = document.getElementById("output-box");
 
+addNewElement("h1", "I'm a regular header!", container, "smaller");
+addNewElement("h3", "I'm smaller but colored.", container, "smaller", "purple");
+addNewElement("p", "This is a very long paragraph element where there are \
+a lot of nice and long words. More than one sentence. I make good use \
+of optional arguments.");
+addNewElement("button", "Press me!");
